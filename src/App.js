fc7b832319fe09b72b6295f0e347df4e658b314c
Alyssa Hooper <nesteralyssa@gmail.com>
2021-03-24 23:01:35 -0500

    Inital get request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './styles/App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get("https://api.hatchways.io/assessment/students")
+        .then(res => {
+          setData(res.data.students)
+    })
+  })
+
+  const listStudents = data.map((data) => {
+    
+  })
+
   return (
     <div className="App">
-      placeholder
+      {listStudents}
     </div>
   );
 }
