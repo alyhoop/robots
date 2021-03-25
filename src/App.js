623c8b562a068ca918968dd16337a243a4d0111a
Alyssa Hooper <nesteralyssa@gmail.com>
2021-03-24 23:28:11 -0500

    Returns json data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Media } from 'react-bootstrap'
 
 function App() {
 
@@ -9,13 +10,28 @@ function App() {
   useEffect(() => {
     axios.get("https://api.hatchways.io/assessment/students")
         .then(res => {
-          setData(res.data.students)
+        setData(res.data.students)
     })
   })
 
-  const listStudents = data.map((data) => {
-    
-  })
+  const listStudents = data.map((students) => (
+    <Media key={students.id}>
+      <img
+        width={64}
+        height={64}
+        className="mr-3"
+        src={students.pic}
+        alt={students.firstName}
+      />
+      <Media.Body>
+        <h5>{students.firstName}{students.lastName}</h5>
+        <p>Email: {students.email}</p>
+        <p>Company: {students.company}</p>
+        <p>Skill: {students.skill}</p>
+        <p>Average: {students.grades}</p>
+      </Media.Body>
+    </Media>
+  ));
 
   return (
     <div className="App">
