4c2d72b1c697871821c04e7d3f77cd71abdaf97e
Alyssa Hooper <nesteralyssa@gmail.com>
2021-03-25 22:06:12 -0500

    adds react bstp import statment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './styles/App.css';
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Media from 'react-bootstrap/Media'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
